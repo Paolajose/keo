687eece1739f0bd1183bb119ee0644175db07e63
paolaj <paoj398@gmail.com>
2021-04-30 16:05:40 -0430

    correcciones

--- src/app/apps/blog/blog-data.ts
+++ src/app/apps/blog/blog-data.ts
@@ -4,83 +4,83 @@ export const blogs: Blog[] = [
     id: 1,
     image: 'assets/images/blog/101.jpg',
     heading:
-      'Magna nostrud incididunt cupidatat cillum incididunt et nisi in ut minim reprehenderit.',
-    subHeading: 'Labore irure irure laborum quis tempor aliqua. Dude',
-    blogDate: 'Feb 19, 2020',
+    'Asociación Chilena de Criptotecnologías integrará el Consejo de la Contraloría de Chile',
+    subHeading: ' Así lo informaron a Cointelegraph en Español, a través de un comunicado',
+    blogDate: 'Abril  28, 2021',
     blogDetail:
-      'Est ipsum sint officia quis nulla nisi cupidatat aliquip nisi laboris eiusmod eiusmod aliquip do. Commodo elit excepteur occaecat irure Lorem sit nulla nulla sint duis incididunt. Nostrud ut do mollit et amet velit aute excepteur et culpa culpa velit. Cillum veniam officia anim cupidatat.',
+      'A través del plan estratégico de la Contraloría chilena, cuyos objetivos contempla promover el trabajo colaborativo y aumentar la participación de la ciudadanía en procesos de alto impacto, se dio a conocer en el presente mes qué asociaciones integrarán su consejo, siendo aceptada la participación de la Asociación Chilena de Criptotecnologías, la cual tiene como objetivo educar y promover el uso de tecnología Blockchain y las criptomonedas”, detallaron en dicho comunicado... Con esto esperamos reforzar nuestra estrategia de incidir en la adopción de tecnología blockchain en organismos públicos del estado, en línea con los objetivos de transparencia y eficiencia que busca la Contraloría',
   },
   {
     id: 2,
     image: 'assets/images/blog/202.jpg',
-    heading: 'Dolore pariatur amet ullamco aliquip mollit aliqua qui.',
+    heading: 'El pulso por la regulación de bitcóin.',
     subHeading:
-      'Eiusmod ipsum do dolore fugiat consequat quis non qui velit consectetur velit.',
-    blogDate: 'Feb 17, 2020',
+      'América Latina es una de las regiones con mayor adopción de esta y otras criptomonedas.',
+    blogDate: 'Abril 17, 2021',
     blogDetail:
-      'Tempor sint cupidatat cillum do ut. Ipsum exercitation est ex id. Sunt ut labore ad nulla.',
+      'Chile no tiene una regulación específica para los criptoactivos. El Banco Central de este país no los considera como “monedas” y la Comisión para el Mercado chileno no las clasifica como “valores”. Sin embargo, sí existe una regulación del Servicio de Impuestos Internos, el cual estableció que a las rentas obtenidas a través de estas operaciones las cobija el artículo 20 de la Ley de Impuesto a la Renta. Además, desde 2018 Chile tiene una ley que regula a las empresas que usen crowdfunding y criptoactivos.',
   },
   {
     id: 3,
     image: 'assets/images/blog/303.jpg',
     heading:
-      'Velit esse dolor dolor qui cillum velit in quis exercitation occaecat.',
+      'Enseña a los niños sobre finanzas, así no sufrirán cuando sean adultos.',
     subHeading:
-      'Adipisicing sit minim occaecat nulla proident exercitation do laboris.',
-    blogDate: 'Feb 15, 2020',
+      '¿Alguna vez te has preguntado el impacto que tiene la forma en la que hablas de dinero en su futuro financiero? ',
+    blogDate: 'Abril 13, 2021',
     blogDetail:
-      'Laboris culpa ea est et aliqua amet ullamco Lorem non enim laborum incididunt ex Lorem. Minim sint cupidatat magna esse labore id. Do qui incididunt consectetur nulla aliquip consequat nostrud incididunt occaecat ad. Excepteur nulla adipisicing enim ea occaecat fugiat dolore sunt est et. Eu veniam nostrud sit aute consectetur dolore ipsum fugiat anim duis.',
+      'Existen muchos métodos que ayudan a los padres y maestros a acercar a los niños y jóvenes a entender cómo funciona el mundo de las finanzas. Con estas cuatro recomendaciones los niños entenderán más sobre finanzas básicas. Es importante enseñarles a los niños que el dinero no sale por magia del cajero automático. El arte de administrar dinero es fácil, sólo deben de comprender desde niños el manejo del capital para no tener una vida llena de inquietudes y frustraciones',
   },
   {
     id: 4,
     image: 'assets/images/blog/404.jpg',
     heading:
-      'Magna nostrud incididunt cupidatat cillum incididunt et nisi in ut minim reprehenderit.',
-    subHeading: 'Labore irure irure laborum quis tempor aliqua. Dude',
-    blogDate: 'Feb 19, 2020',
+      'Turquía se convierte en la mayor economía del mundo que prohíbe las criptomonedas',
+    subHeading: 'Revés a las criptomonedas desdeTurquía.',
+    blogDate: 'Abril 16, 2021',
     blogDetail:
-      'Est ipsum sint officia quis nulla nisi cupidatat aliquip nisi laboris eiusmod eiusmod aliquip do. Commodo elit excepteur occaecat irure Lorem sit nulla nulla sint duis incididunt. Nostrud ut do mollit et amet velit aute excepteur et culpa culpa velit. Cillum veniam officia anim cupidatat.',
+      'El banco central del país prohibió los pagos directos e indirectos con criptomonedas y criptoactivos, en una inusual posición sobre el mercado de monedas virtual. En un anuncio publicado en el boletín oficial del Estado, el ente justifica su decisión en los riesgos de las transacciones."Los proveedores de servicios de pago no podrán llevar a cabo sus transacciones de forma que los criptoactivos se usen de forma directa o indirecta en la prestación de servicios de pago y emisión de dinero electrónico", señala el comunicado..',
   },
   {
     id: 5,
     image: 'assets/images/blog/505.jpg',
-    heading: 'Dolore pariatur amet ullamco aliquip mollit aliqua qui.',
+    heading: 'Binance lanza tokens de acciones de Apple, Microsoft y MicroStrategy.',
     subHeading:
-      'Eiusmod ipsum do dolore fugiat consequat quis non qui velit consectetur velit.',
-    blogDate: 'Feb 17, 2020',
+      'El comercio de estos tokens seguirá los horarios tradicionales de negociación de la bolsa.',
+    blogDate: 'Abril 15, 2021',
     blogDetail:
-      'Tempor sint cupidatat cillum do ut. Ipsum exercitation est ex id. Sunt ut labore ad nulla.',
+      'Ahora es posible invertir en Apple, Microsoft y MicroStrategy desde el mayor exchange de bitcoin y criptomonedas, Binance. La empresa asiática anunció el lanzamiento de tokens que siguen el precio de las acciones bursátiles de estas compañías este lunes 26 de abril. Los tokens de acciones de Binance permiten a sus usuarios negociar fracciones de acciones bursátiles de diversas compañías que cotizan en bolsa sin comisiones, explicó el exchange a través de un breve comunicado anunciando los tres tokens lanzados..',
   },
-  {
-    id: 6,
-    image: 'assets/images/blog/707.jpg',
-    heading:
-      'Velit esse dolor dolor qui cillum velit in quis exercitation occaecat.',
-    subHeading:
-      'Adipisicing sit minim occaecat nulla proident exercitation do laboris.',
-    blogDate: 'Feb 15, 2020',
-    blogDetail:
-      'Laboris culpa ea est et aliqua amet ullamco Lorem non enim laborum incididunt ex Lorem. Minim sint cupidatat magna esse labore id. Do qui incididunt consectetur nulla aliquip consequat nostrud incididunt occaecat ad. Excepteur nulla adipisicing enim ea occaecat fugiat dolore sunt est et. Eu veniam nostrud sit aute consectetur dolore ipsum fugiat anim duis.',
-  },
-  {
-    id: 7,
-    image: 'assets/images/blog/808.jpg',
-    heading: 'Dolore pariatur amet ullamco aliquip mollit aliqua qui.',
-    subHeading:
-      'Eiusmod ipsum do dolore fugiat consequat quis non qui velit consectetur velit.',
-    blogDate: 'Feb 17, 2020',
-    blogDetail:
-      'Tempor sint cupidatat cillum do ut. Ipsum exercitation est ex id. Sunt ut labore ad nulla.',
-  },
-  {
-    id: 8,
-    image: 'assets/images/blog/909.jpg',
-    heading:
-      'Velit esse dolor dolor qui cillum velit in quis exercitation occaecat.',
-    subHeading:
-      'Adipisicing sit minim occaecat nulla proident exercitation do laboris.',
-    blogDate: 'Feb 15, 2020',
-    blogDetail:
-      'Laboris culpa ea est et aliqua amet ullamco Lorem non enim laborum incididunt ex Lorem. Minim sint cupidatat magna esse labore id. Do qui incididunt consectetur nulla aliquip consequat nostrud incididunt occaecat ad. Excepteur nulla adipisicing enim ea occaecat fugiat dolore sunt est et. Eu veniam nostrud sit aute consectetur dolore ipsum fugiat anim duis.',
-  },
+//   {
+//     id: 6,
+//     image: 'assets/images/blog/707.jpg',
+//     heading:
+//       'Velit esse dolor dolor qui cillum velit in quis exercitation occaecat.',
+//     subHeading:
+//       'Adipisicing sit minim occaecat nulla proident exercitation do laboris.',
+//     blogDate: 'Feb 15, 2020',
+//     blogDetail:
+//       'Laboris culpa ea est et aliqua amet ullamco Lorem non enim laborum incididunt ex Lorem. Minim sint cupidatat magna esse labore id. Do qui incididunt consectetur nulla aliquip consequat nostrud incididunt occaecat ad. Excepteur nulla adipisicing enim ea occaecat fugiat dolore sunt est et. Eu veniam nostrud sit aute consectetur dolore ipsum fugiat anim duis.',
+//   },
+//   {
+//     id: 7,
+//     image: 'assets/images/blog/808.jpg',
+//     heading: 'Dolore pariatur amet ullamco aliquip mollit aliqua qui.',
+//     subHeading:
+//       'Eiusmod ipsum do dolore fugiat consequat quis non qui velit consectetur velit.',
+//     blogDate: 'Feb 17, 2020',
+//     blogDetail:
+//       'Tempor sint cupidatat cillum do ut. Ipsum exercitation est ex id. Sunt ut labore ad nulla.',
+//   },
+//   {
+//     id: 8,
+//     image: 'assets/images/blog/909.jpg',
+//     heading:
+//       'Velit esse dolor dolor qui cillum velit in quis exercitation occaecat.',
+//     subHeading:
+//       'Adipisicing sit minim occaecat nulla proident exercitation do laboris.',
+//     blogDate: 'Feb 15, 2020',
+//     blogDetail:
+//       'Laboris culpa ea est et aliqua amet ullamco Lorem non enim laborum incididunt ex Lorem. Minim sint cupidatat magna esse labore id. Do qui incididunt consectetur nulla aliquip consequat nostrud incididunt occaecat ad. Excepteur nulla adipisicing enim ea occaecat fugiat dolore sunt est et. Eu veniam nostrud sit aute consectetur dolore ipsum fugiat anim duis.',
+//   },
 ];
